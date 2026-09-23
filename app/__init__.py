099031118038bfe7994b6ee71b7f6eec45de31ce
Zander <zrbreton@waimea.school.nz>
2026-09-24 09:27:49 +1200

    demo-page added
    - More changes changes changessss

--- app/__init__.py
+++ app/__init__.py
@@ -25,22 +25,13 @@ app = Flask(__name__)
 #-----------------------------------------------------------
 @app.get("/")
 def show_notes():
-    with connect_db() as db:
-        sql = """
-            SELECT id, title, body, pinned, created
-            FROM note
-            ORDER BY pinned DESC, created DESC
-        """
-        params = ()
-        notes = db.execute(sql, params).fetchall()
-
         flash("Test message")
         flash("Test SUCCESS message", "success")
         flash("Test INFO message", "info")
         flash("Test WARNING message", "warning")
         flash("Test ERROR message", "error")
-
-        return render_template("pages/index.jinja", notes=notes)
+        
+        return render_template("pages/index.jinja")
 
 #===========================================================
 # Import - Shows a text box for easy pasting 
